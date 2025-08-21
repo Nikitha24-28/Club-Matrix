7542b85217b52310ecc1d4477ddf7d897f64b3da
Nikitha G <gnikitha.ad24@bitsathy.ac.in>
2025-08-21 22:25:12 +0530

    Basic Landing page and working of NavBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,44 @@
 import React from 'react';
-import AppLayout from './components/AppLayout/AppLayout';
-import MoM from "./pages/Client/Coordinator/MoM/MoM";
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import ClientLayout from './pages/Client/Nav/ClientLayout';
 import CommunityPage from "./pages/Client/Common/CommunityPage/CommunityPage";
+import CreateClub from "./pages/Client/Common/CreateClub/CreateClub";
+import PriorLogin from "./pages/priorlogin/priorlogin";
 
 const App = () => {
   return (
-    <div>
-      {/* <AppLayout/> */}
-      {/* <MoM/> */}
-      <CommunityPage/>
-    </div>
+    <PriorLogin/>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Navigate to="/CommunityPage" />} />
+    //     <Route path="/CommunityPage" element={
+    //       <ClientLayout userRole="general">
+    //         <CommunityPage />
+    //       </ClientLayout>
+    //     } />
+    //     <Route path="/CreateClubPage" element={
+    //       <ClientLayout userRole="general">
+    //         <CreateClub />
+    //       </ClientLayout>
+    //     } />
+    //     <Route path="/ProfilePage" element={
+    //       <ClientLayout userRole="general">
+    //         <div>Profile Page - Coming Soon</div>
+    //       </ClientLayout>
+    //     } />
+    //     <Route path="/JoinRequestsPage" element={
+    //       <ClientLayout userRole="general">
+    //         <div>Join Requests Page - Coming Soon</div>
+    //       </ClientLayout>
+    //     } />
+    //     <Route path="/SettingsPage" element={
+    //       <ClientLayout userRole="general">
+    //         <div>Settings Page - Coming Soon</div>
+    //       </ClientLayout>
+    //     } />
+    //     <Route path="*" element={<Navigate to="/CommunityPage" />} />
+    //   </Routes>
+    // </Router>
   )
 }
 
--- frontend/src/pages/Client/Nav/IntegrationExample.jsx
+++ frontend/src/pages/Client/Nav/IntegrationExample.jsx
@@ -125,3 +125,4 @@ export const UpdatedAnnouncementsExample = () => {
     </PageWrapper>
   );
 };
+
